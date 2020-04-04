845443c6c8096023401c349560047195e5472906
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2020-04-05 00:05:36 +0200

    refactor: Simplify entrypoint.sh by using $INPUT_* instead of args

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,51 +18,36 @@ echorun() {
     echo "$ $*"
     "$@"
 }
+echorun cat /etc/os-release || true
 echorun cat /proc/cpuinfo || true
 echorun cat /proc/meminfo || true
-echorun cat /etc/os-release || true
 
 endGroup
-
-## Initial values
-PACKAGE=""
-_OPAM_FILE=""
-_COQ_VERSION=""
-_OCAML_VERSION=""
-_SCRIPT=""
 
 usage() {
     cat <<EOF
 Usage:
-  $0 -f fil.opam -c 8.11 -m 4.05 -s ''
+  INPUT_OPAM_FILE=file.opam \\
+  INPUT_COQ_VERSION=8.11 \\
+  INPUT_OCAML_VERSION=minimal \\
+  INPUT_CUSTOM_SCRIPT='...' \\
+  $0
 
 Options:
--f OPAM_FILE: The path of the .opam file, relative to the repo root.
--c COQ_VERSION: The minor version of Coq.
--m OCAML_VERSION: The minor version of OCaml (4.05, 4.07-flambda, 4.09-flambda)
--s SCRIPT: The main script run in the container.
+INPUT_OPAM_FILE: the path of the .opam file, relative to the repo root
+INPUT_COQ_VERSION: the version of Coq (without patch-level)
+INPUT_OCAML_VERSION: the version of OCaml (minimal, 4.07-flambda, 4.09-flambda)
+INPUT_CUSTOM_SCRIPT: the main script run in the container
 EOF
 }
 
 ## Parse options
 OPTIND=1 # Reset is necessary if getopts was used previously in the script.  It is a good idea to make this local in a function.
-while getopts "hf:c:m:s:" opt; do
+while getopts "h" opt; do
     case "$opt" in
         h)
             usage
             exit 0
-            ;;
-        f)
-            _OPAM_FILE="$OPTARG"
-            ;;
-        c)
-            _COQ_VERSION="$OPTARG"
-            ;;
-        m)
-            _OCAML_VERSION="$OPTARG"
-            ;;
-        s)
-            _SCRIPT="$OPTARG"
             ;;
         '?')
             usage
@@ -76,45 +61,45 @@ if test $# -gt 0; then
     echo "Warning: Arguments ignored: $*"
 fi
 
-if test -z "$_OPAM_FILE"; then
+if test -z "$INPUT_OPAM_FILE"; then
     echo "ERROR: No opam file specified."
     usage
     exit 1
 fi
 
-if test -z "$_COQ_VERSION"; then
+if test -z "$INPUT_COQ_VERSION"; then
     echo "ERROR: No Coq version specified."
     usage
     exit 1
 fi
 
-if test -z "$_OCAML_VERSION"; then
+if test -z "$INPUT_OCAML_VERSION"; then
     echo "ERROR: No OCaml version specified."
     usage
     exit 1
 fi
 
-if test -z "$_SCRIPT"; then
+if test -z "$INPUT_CUSTOM_SCRIPT"; then
     echo "ERROR: The specified script is empty."
     usage
     exit 1
 fi
 
-PACKAGE=${_OPAM_FILE%.opam}
+PACKAGE=${INPUT_OPAM_FILE%.opam}
 PACKAGE=${PACKAGE##*/}
 # todo: test pinning when _OPAM_FILE contains a '/'
 
-# TODO: validation of COQ_VERSION, OCAML_VERSION
+# TODO: validation of INPUT_COQ_VERSION, INPUT_OCAML_VERSION
 
-COQ_IMAGE="coqorg/coq:$_COQ_VERSION"
+COQ_IMAGE="coqorg/coq:$INPUT_COQ_VERSION"
 
 # todo: update this after the one-switch docker-coq migration
 OCAML407="false"
-if [ "$_OCAML_VERSION" = '4.09-flambda' ]; then
+if [ "$INPUT_OCAML_VERSION" = '4.09-flambda' ]; then
     COQ_IMAGE="${COQ_IMAGE}-ocaml-4.09.0-flambda"
-elif [ "$_OCAML_VERSION" = '4.07-flambda' ]; then
+elif [ "$INPUT_OCAML_VERSION" = '4.07-flambda' ]; then
     OCAML407="true"
-# else Assume "$_OCAML_VERSION" = 'minimal'
+# else Assume "$INPUT_OCAML_VERSION" = 'minimal'
 fi
 echo OCAML407="$OCAML407"
 
@@ -152,6 +137,6 @@ docker run -i --init --rm --name=COQ -e PACKAGE="$PACKAGE" \
 endGroup () {  {  init_opts=\"\$-\"; set +x ; } 2> /dev/null; if [ -n \"\$startTime\" ]; then endTime=\$(date -u +%s); echo \"::endgroup::\"; printf \"↳ \"; date -u -d \"@\$((endTime - startTime))\" '+%-Hh %-Mm %-Ss'; echo; unset startTime; else echo 'Error: missing startGroup command.'; case \"\$init_opts\" in  *x*) set -x ;; esac; return 1; fi; case \"\$init_opts\" in  *x*) set -x ;; esac; } ; startGroup () {  {  init_opts=\"\$-\"; set +x ; } 2> /dev/null; if [ -n \"\$startTime\" ]; then endGroup; fi; if [ \$# -ge 1 ]; then groupTitle=\"\$*\"; else groupTitle=\"Unnamed group\"; fi; echo; echo \"::group::\$groupTitle\"; startTime=\$(date -u +%s); case \"\$init_opts\" in  *x*) set -x ;; esac; } # generated from helper.sh
 export PS4='+ \e[33;1m(\$0 @ line \$LINENO) \$\e[0m '; set -ex
 $_OCAML407_COMMAND
-$_SCRIPT" script
+$INPUT_CUSTOM_SCRIPT" script
 
 echo "done"
